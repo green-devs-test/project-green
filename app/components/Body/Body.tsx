6d0c40707c3d4f913d5249f8ad3f0dba8a648532
dev <jafibravin@outlook.com>
2024-01-17 14:10:48 -0300

    initial setup

---
+++ app/components/Body/Body.tsx
@@ -0,0 +1,10 @@
+import type { MetaFunction } from "@remix-run/node";
+
+
+export default function Body() {
+  return (
+    <div>
+      <p>Body goes here</p>
+    </div>
+  );
+}
---
+++ app/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,10 @@
+import type { MetaFunction } from "@remix-run/node";
+
+
+export default function Sidebar() {
+  return (
+    <div>
+      <p>Sidebar goes here</p>
+    </div>
+  );
+}
