2db99fd744cb73a75f5909a9540d7467e1f7f51b
Gustavo Iky Robles <the.serviceace@gmail.com>
2024-02-12 11:49:01 -0300

    agregado un componente para estilos para imagenes

---
+++ src/Components/Imagen/Imagen.tsx
@@ -0,0 +1,12 @@
+import styles from './styles';
+import { css } from "aphrodite";
+
+const Imagen = () => {
+    return (
+        <div className={css(styles.imagen)}>
+            <img src=""></img>
+        </div>
+    );
+}
+
+export default Imagen;
---
+++ src/Components/Imagen/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "aphrodite";
+
+const styles = StyleSheet.create({
+    imagen: {
+        width: 'auto',
+        height: 'auto',
+        border: '4px solid black',
+        borderBottom: '20px solid black',
+        borderRadius: '12px 12px 0 0',
+    },
+});
+
+export default styles;
