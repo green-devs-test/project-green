3157d81ea4d2367fe1ff5fc6287deedad893cebd
Gustavo Iky Robles <the.serviceace@gmail.com>
2024-02-14 06:54:25 -0300

    arreglos en los estilos del body

--- src/Components/Body/Body.tsx
+++ src/Components/Body/Body.tsx
@@ -1,10 +1,11 @@
 import { css } from 'aphrodite';
 import styles from './styles';
+import Link from '../Link';
 
 
 const Body = () => {
   return (
-    <div className={css(styles.body)}> Body </div>
+    <div className={css(styles.body)}> <Link /> <Link /> <Link /> </div>
   );
 }
 
