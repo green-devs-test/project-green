2c7d5bc837d9690bc3d539d739f28b3194573030
Gustavo Iky Robles <the.serviceace@gmail.com>
2024-04-04 02:41:14 -0300

    Generated the link between the GoUp component and the TopBar.

--- src/Components/GoUpButton/GoUpButton.tsx
+++ src/Components/GoUpButton/GoUpButton.tsx
@@ -1,12 +1,17 @@
 import { css } from "aphrodite";
 import styles from "./styles";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+
 
 const GoUpButton = () => {
     return (
         <>
-            <div className={css(styles.goUpButton)}>
-                ⬆️
-            </div>
+            <a href="#top">
+                <div className={css(styles.goUpButton)}>
+                    <FontAwesomeIcon icon={faArrowUp} className={css(styles.icon)}/>
+                </div>
+            </a>
         </>
     );
 };
