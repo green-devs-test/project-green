6dbcfe04cfce79c04b8647909f460908956a6f16
Gustavo Iky Robles <the.serviceace@gmail.com>
2024-04-05 10:35:20 -0300

    Removed unused components and also commented styles (#23)

--- src/Components/GoUpButton/styles.ts
+++ src/Components/GoUpButton/styles.ts
@@ -6,7 +6,6 @@ const styles = StyleSheet.create({
         width: 40,
         height: 40,
         borderRadius: 50,
-        overflow: 'hidden', //delete when necessary
         border: '2px solid black',
         position: 'fixed',
         zIndex: 12,
