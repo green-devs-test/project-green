df864e0852fd8a965ad6f049a68172d165618383
Gustavo Iky Robles <the.serviceace@gmail.com>
2024-04-10 14:05:53 -0300

    Making SpotBradcrumb component and adding it to SearchOptions

--- src/Components/SearchOptions/SearchOptions.tsx
+++ src/Components/SearchOptions/SearchOptions.tsx
@@ -2,6 +2,7 @@ import styles from "./styles";
 import { css } from "aphrodite";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSliders } from "@fortawesome/free-solid-svg-icons";
+import SpotBreadcrumb from "../SpotBreadcrumb";
 
 interface SearchOptionsProps {
   results: number;
@@ -14,10 +15,7 @@ const SearchOptions = (props: SearchOptionsProps) => {
         <p className={css(styles.numbersOfResults)}>
           <b>{props.results}</b> Resultado{props.results > 1 ? "s" : ""}
         </p>
-        <button className={css(styles.searchOptions)} type="button">
-          Filtros{" "}
-          <FontAwesomeIcon icon={faSliders} className={css(styles.icon)} />
-        </button>
+        <SpotBreadcrumb />
       </div>
     </>
   );
---
+++ src/Components/SpotBreadcrumb/SpotBreadcrumb.tsx
@@ -0,0 +1,12 @@
+import styles from "./styles";
+import { css } from "aphrodite";
+
+const SpotBreadcrumb = () => {
+  return (
+    <div className={css(styles.test)}>
+      <p>CABA &lt; Almagro</p>
+    </div>
+  );
+};
+
+export default SpotBreadcrumb;
