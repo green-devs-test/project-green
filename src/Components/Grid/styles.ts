5a0ee45562d73abc8a96a9a5bca6311405965c47
jafidiesel <jafibravin@outlook.com>
2024-04-30 16:01:35 -0300

    [fe-019] Basic style for no results

--- src/Components/Grid/styles.ts
+++ src/Components/Grid/styles.ts
@@ -1,5 +1,16 @@
 import { StyleSheet } from "aphrodite";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    bold: {
+        fontWeight: "bold",
+    },
+    centerGrid: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        height: "100%",
+    }
+});
 
 export default styles;
