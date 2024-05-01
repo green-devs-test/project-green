38c8ef57d847e672745f4dbe8981fd9ec019feb7
Jafi <jafidiesel@users.noreply.github.com>
2024-05-01 00:09:28 -0300

    [fe-019] Basic style for no results (#35)

--- src/Components/Grid/Grid.tsx
+++ src/Components/Grid/Grid.tsx
@@ -1,11 +1,20 @@
+import { css } from "aphrodite";
 import { Spot } from "../../Services/GeoLocality.interfaces";
 import Card from "../Card";
+import styles from "./styles";
 
 interface GridProps {
   spots: Spot[] | null;
 }
 
 const Grid = (props: GridProps) => {
+  if (!props.spots || !props.spots.length)
+    return (
+      <div className={css(styles.centerGrid)}>
+        <p className={css(styles.bold)}>No se encontraron resultados.</p>
+        <p>Probá con otra localidad.</p>
+      </div>
+    );
   return (
     <>
       {props.spots?.map((spot) => <Card spot={spot} key={`spot-${spot.id}`} />)}
